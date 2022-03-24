8871991f28eab38e78941f6ab3414e2a41c972ff
weerawichgithub <Weerawich@gmail.com>
2022-03-24 13:05:00 -0400

    rename goLogin method to doLogin

--- src/test/java/pages/loginPage.java
+++ src/test/java/pages/loginPage.java
@@ -10,7 +10,6 @@ public class loginPage {
 
     public loginPage() {PageFactory.initElements(Driver.getDriver(),this);}
 
-
     @FindBy(id="txtUsername")
     public WebElement usernameLoginInput;
 
